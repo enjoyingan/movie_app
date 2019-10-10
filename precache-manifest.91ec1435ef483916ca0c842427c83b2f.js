b09e767234205d3d348de63b08366f42d649bc63
enjoyingan <parkhoon789@naver.com>
2019-10-10 19:47:08 +0900

    Updates

---
+++ precache-manifest.91ec1435ef483916ca0c842427c83b2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7702068e8818e8d01e8fb1394a52ef5b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4e2c01fef467c3a43262",
+    "url": "/movie_app/static/css/main.21b1ad61.chunk.css"
+  },
+  {
+    "revision": "ca6d0ab03409f96801db",
+    "url": "/movie_app/static/js/2.5c162f41.chunk.js"
+  },
+  {
+    "revision": "4e2c01fef467c3a43262",
+    "url": "/movie_app/static/js/main.fd734e06.chunk.js"
+  },
+  {
+    "revision": "8fbd9badc7454a983c4d",
+    "url": "/movie_app/static/js/runtime~main.9f434370.js"
+  }
+]);
